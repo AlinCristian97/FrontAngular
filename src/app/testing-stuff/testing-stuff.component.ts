06feef1b633f139a7e80dbf15cba0fff335a4feb
Alin Cristian <alin.cristian977@gmail.com>
2023-06-22 00:34:24 +0300

    added multiple environment settings

--- src/app/testing-stuff/testing-stuff.component.ts
+++ src/app/testing-stuff/testing-stuff.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-testing-stuff',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class TestingStuffComponent {
 
+  constructor() { }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    backendApiUrl: 'http://localhost:5241',
+    environmentName: 'development'
+  };
