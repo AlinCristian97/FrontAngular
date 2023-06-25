d0debb0be7f8b370734dbed01cb80b464449f9c9
Alin <alin.cristian977@gmail.com>
2023-06-25 10:59:51 +0300

    small updates

--- src/app/components/learn/learn-tooltips/learn-tooltips.component.ts
+++ src/app/components/learn/learn-tooltips/learn-tooltips.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ComponentBase } from 'src/app/shared/classes/component-base';
 
 @Component({
   selector: 'app-learn-tooltips',
   templateUrl: './learn-tooltips.component.html',
   styleUrls: ['./learn-tooltips.component.css']
 })
-export class LearnTooltipsComponent {
+export class LearnTooltipsComponent extends ComponentBase {
 
 }
--- src/app/components/testing-stuff/testing-stuff.component.ts
+++ src/app/components/testing-stuff/testing-stuff.component.ts
@@ -12,5 +12,4 @@ export class TestingStuffComponent extends ComponentBase {
     super();
   }
 
-
 }
