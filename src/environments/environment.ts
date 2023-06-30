198f7116f1b9f503ca06f1d0e32ad3d64239efc9
Alin Cristian <alin.cristian977@gmail.com>
2023-06-30 09:06:52 +0300

    added http endpoint only for dev environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    backendApiUrl: 'https://localhost:7175/api',
+    // backendApiUrl: 'https://localhost:7175/api',
+    backendApiUrl: 'http://localhost:5241/api',
     environmentName: 'development'
   };
